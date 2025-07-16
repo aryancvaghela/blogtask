1652861c58b45686ee10033eb91fb36d2351e91a
Aryan Vaghela <aryancvaghela@gmail.com>
2025-07-16 19:26:32 +0530

    blog

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
    <div class="col-md-4 mb-4">
      <div class="card h-100">
        @if($blog->image)
-         <img src="{{ Storage::url($blog->image) }}" class="card-img-top" style="height:180px;object-fit:cover">
+         <img src="{{ Storage::url($blog->image[0]) }}" class="card-img-top" style="margin-left:10%;height:180px;width:130px;object-fit:cover">
        @endif
        <div class="card-body">
          <h5 class="card-title">{{ $blog->title }}</h5>
